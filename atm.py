c4337af23f34cf191081147b6a002472015951c5
Kush321 <78313670+Kush321@users.noreply.github.com>
2021-11-04 21:12:22 -0400

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,11 @@
+class ATM:
+    def __init__(self,cardnumber,pin):
+        self.cardnumber = ""
+        self.pin = ""
+    def withdraw():
+        print("Withdraw")
+    def balance():
+        print("Balance")
+money = ATM(input("Cardnumber: "), input("Pin: "))
+ATM.withdraw()
+ATM.balance()
